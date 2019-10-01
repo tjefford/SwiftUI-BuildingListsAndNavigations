0d0ee62d30b1c718f6f93782c585a464fddb99c3
Tyler Jefford <tylerjefford@gmail.com>
2019-09-30 20:58:11 -0500

    Initial Commit

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Tyler Jefford on 9/30/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
